3f436e0d7ed47a98e7230e687ba6e8ab6257022e
Den <denysdiachenko1997@gmail.com>
2020-01-22 16:41:45 +0200

    Category table & Category Cell

--- coder-swag/Controller/CategoryVC.swift
+++ coder-swag/Controller/CategoryVC.swift
@@ -8,8 +8,12 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class CategoryVC: UIViewController {
 
+    @IBOutlet weak var categoryTable: UITableView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
