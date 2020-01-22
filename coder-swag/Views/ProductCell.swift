25895a744117df0e8c512951771983e360aceb1b
Den <denysdiachenko1997@gmail.com>
2020-01-22 19:37:14 +0200

    Collection View

---
+++ coder-swag/Views/ProductCell.swift
@@ -0,0 +1,18 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by Ден on 22.01.2020.
+//  Copyright © 2020 Ден. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+    
+    
+}
